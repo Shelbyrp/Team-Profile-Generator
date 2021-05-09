66cb22d8481eea57571aba7d9626e9be9927dce8
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-05-09 13:45:56 +0800

    Created an if else statement

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ const question = [
     type: "list",
     name: "type",
     message: "Add another team member?",
-    choices: [{name: "Engineer", value: 0}, {name: "Intern", value: 1}, {name: "Manager", value: 2}, {name: "No - finish building my team", value: 3}],
+    choices: [
+      { name: "Engineer", value: 0 },
+      { name: "Intern", value: 1 },
+      { name: "Manager", value: 2 },
+      { name: "No - finish building my team", value: 3 },
+    ],
   },
 ];
 
@@ -75,33 +80,35 @@ function init() {
     .then((team) => {
       const report1 = generateReport.generateReport(team);
       writeToFile(filename, report1);
-     // Nest 2 - prompt to add more team members
+      // Nest 2 - prompt to add more team members
       inquirer
         .prompt(question)
         .then((responses) => {
-            if (responses.type === 0){
+          if (responses.type === 0) {
             const positionOfStaff = "Engineer";
             console.log("GOT PAST ENGINEER", positionOfStaff);
-            } else if (responses.type === 1){
-                const positionOfStaff = "Intern";
-            } else if (responses.type === 2){
-                const positionOfStaff = "Manager";
-            } else {
-                return "No - finish building my team";
-            };
-            console.log("RESULT: ", positionOfStaff);
-            //Nest 3 - prompt for additional team member information
-            inquirer
-              .prompt(addTeamMembers)
-              .then((result2) => {
-                console.log("RESULT: ", result2);
-              })
-              .catch((err) => console.log("NEGAITIVE RESULT #3 !!!"));
-        })
+          } else if (responses.type === 1) {
+            const positionOfStaff = "Intern";
+            console.log("GOT PAST INTERN", positionOfStaff);
+          } else if (responses.type === 2) {
+            const positionOfStaff = "Manager";
+            console.log("GOT PAST MANAGER", positionOfStaff);
+          } else if (responses.type === 3) {
+            console.log("To exit");
+            return;
+          } else {
+          //Nest 3 - prompt for additional team member information
+          inquirer
+            .prompt(addTeamMembers)
+            .then((result2) => {
+              console.log("RESULT: ", result2);
+            })
+            .catch((err) => console.log("NEGAITIVE RESULT #3 !!!"));
+        }})
         .catch((err) => console.log("NEGAITIVE RESULT #2 !!!"));
     })
     .catch((err) => console.log("NEGAITIVE RESULT #1 !!!"));
-};
+}
 
 // This init the function to invoke the app
 const filename = "test.txt";
