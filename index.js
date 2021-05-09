a4ac573ed58cca3ec3badecde2a3c672363c3e7d
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-05-09 14:50:34 +0800

    Fixed the if statements

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateReport = require("./lib/generateReport");
 
+const questionTeamManager = [
+  {
+    type: "confirm",
+    name: "leader",
+    message: "Are you the team leader?",
+  },
+];
+
 const addTeamManager = [
   {
     type: "input",
@@ -23,14 +31,9 @@ const addTeamManager = [
     name: "office",
     message: "What is your office number?",
   },
-  {
-    type: "confirm",
-    name: "position",
-    message: "Are you the team leader?",
-  },
 ];
 
-const question = [
+const questionMoreTeamMembers = [
   {
     type: "list",
     name: "type",
@@ -75,39 +78,52 @@ function writeToFile(filename, report1) {
 
 function init() {
   // Nest 1 - Get team leader information
-  inquirer
-    .prompt(addTeamManager)
-    .then((team) => {
-      const report1 = generateReport.generateReport(team);
-      writeToFile(filename, report1);
-      // Nest 2 - prompt to add more team members
+  inquirer.prompt(questionTeamManager).then((response) => {
+    if (!response.leader) {
+        console.log("Please ask your team leader to complete this form");
+        return;
+      };
+      // Nest 2 - Get team leader information
       inquirer
-        .prompt(question)
-        .then((responses) => {
-          if (responses.type === 0) {
-            const positionOfStaff = "Engineer";
-            console.log("GOT PAST ENGINEER", positionOfStaff);
-          } else if (responses.type === 1) {
-            const positionOfStaff = "Intern";
-            console.log("GOT PAST INTERN", positionOfStaff);
-          } else if (responses.type === 2) {
-            const positionOfStaff = "Manager";
-            console.log("GOT PAST MANAGER", positionOfStaff);
-          } else if (responses.type === 3) {
-            console.log("To exit");
-            return;
-          } else {
-          //Nest 3 - prompt for additional team member information
+        .prompt(addTeamManager)
+        .then((response) => {
+          response.position = "Team Manager";
+          const report1 = generateReport.generateReport(response);
+          writeToFile(filename, report1);
+          console.log(response.position);
+          console.log("RESULT: ", response);
+          // Nest 3 - prompt to add more team members
           inquirer
-            .prompt(addTeamMembers)
-            .then((result2) => {
-              console.log("RESULT: ", result2);
+            .prompt(questionMoreTeamMembers)
+            .then((response) => {
+              if (response.type === 3) {
+                console.log("To exit");
+                return;
+              } else {
+                if (response.type === 0) {
+                  var positionOfStaff = "Engineer";
+                }
+                if (response.type === 1) {
+                  var positionOfStaff = "Intern";
+                }
+                if (response.type === 2) {
+                  var positionOfStaff = "Manager";
+                }
+                //Nest 4 - prompt for additional team member information
+                inquirer
+                  .prompt(addTeamMembers)
+                  .then((response) => {
+                    response["position"] = positionOfStaff;
+                    console.log("RESULT: ", response);
+                  })
+                  .catch((err) => console.log("NEGAITIVE RESULT #3 !!!"));
+              }
             })
-            .catch((err) => console.log("NEGAITIVE RESULT #3 !!!"));
-        }})
-        .catch((err) => console.log("NEGAITIVE RESULT #2 !!!"));
-    })
-    .catch((err) => console.log("NEGAITIVE RESULT #1 !!!"));
+            .catch((err) => console.log("NEGAITIVE RESULT #2 !!!"));
+        })
+        .catch((err) => console.log("NEGAITIVE RESULT #1 !!!"));
+    }
+  )
 }
 
 // This init the function to invoke the app
